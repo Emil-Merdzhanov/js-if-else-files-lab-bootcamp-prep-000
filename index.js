f48d8f1aecf4bb2f0ec5eebb9b7077f644cfcb3c
decent-port-4955 <emil@georgetownoliveoil.com>
2019-08-22 03:30:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Susan"
 var height = "74"
 
-if (name === "susan") {console.log ("the name is correct")}
+if (name === "Susan") {console.log ("the name is correct")}
 else {console.log ("If a woodchuck could chuck wood, how much wood could a woodchuck chuck if a woodchuck was Chuck Norris ")}
 
 
