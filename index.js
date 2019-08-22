7ed12fde1735581e4b0ecd7394981a6d211343a3
decent-port-4955 <emil@georgetownoliveoil.com>
2019-08-22 03:31:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Susan") {console.log ("the name is correct")}
-else {console.log ("If a woodchuck could chuck wood, how much wood could a woodchuck chuck if a woodchuck was Chuck Norris ")}
-
-
-
 // Don't worry about this
 module.exports = { name, height
 }
