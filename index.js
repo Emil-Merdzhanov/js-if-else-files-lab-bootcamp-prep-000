ac25d6f4e459f5025b9cee50ba92f36f01662ab5
decent-port-4955 <emil@georgetownoliveoil.com>
2019-08-22 03:26:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "susan") {"the name is correct"}
+if (name === "susan") {console.log ("the name is correct")}
+else {console.log ("Expected the name to be Susan")}
+
 
 
 // Don't worry about this
